fde08ca4dd0c516146924893c356a4e057ed7669
aleksandrgarshin <mail@mail.ru>
2017-06-07 18:03:17 +0300

    lab1.3

--- Lab1.3/snmp.py
+++ Lab1.3/snmp.py
@@ -1,32 +1,37 @@
 from pysnmp.hlapi import *
 
 community_name = "public"
-
 ipaddr_string = "10.31.70.107"
 port_int = 161
 
+def getSNMP(obj):
+    return getCmd(SnmpEngine(),
+                     CommunityData(community_name, mpModel=0),
+                     UdpTransportTarget((ipaddr_string, port_int)),
+                     ContextData(),
+                     ObjectType(obj))
+
+def getNextSNMP(obj):
+    return nextCmd(SnmpEngine(),
+                     CommunityData(community_name, mpModel=0),
+                     UdpTransportTarget((ipaddr_string, port_int)),
+                     ContextData(),
+                     ObjectType(obj),
+                     lexicographicMode=False)
+
 #GET IOS VERSION
-result1 = getCmd(SnmpEngine(),
-                CommunityData(community_name, mpModel=0),
-                UdpTransportTarget((ipaddr_string, port_int)),
-                ContextData(),
-                ObjectType(ObjectIdentity('SNMPv2-MIB', 'sysDescr', 0)))
+result1 = getSNMP(ObjectIdentity('SNMPv2-MIB', 'sysDescr', 0))
 
 for str in result1:
     errorIndication, errorStatus, errorIndex, varBinds = str
-    for str2 in varBinds:
-        print(str2)
+    for str2 in varBinds: print(str2[1])
 
 print("")
+
 #GET INTERFACES
-result2 = nextCmd(SnmpEngine(),
-                CommunityData(community_name, mpModel=0),
-                UdpTransportTarget((ipaddr_string, port_int)),
-                ContextData(),
-                ObjectType(ObjectIdentity('1.3.6.1.2.1.2.2.1.2')),
-                lexicographicMode = False)
+result2 = getNextSNMP(ObjectIdentity('1.3.6.1.2.1.2.2.1.2'))
 
 for str in result2:
     varBinds = str[3]
     for str2 in varBinds:
-        print(str2)
+        print(str2[1])
