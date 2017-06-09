b9908f70f9b3132b799e65f534c4ebd6be5aadc7
aleksandrgarshin <mail@mail.ru>
2017-06-09 17:38:14 +0300

    lab1.4

--- Lab1.5/file.py
+++ Lab1.5/file.py
@@ -1,4 +1,5 @@
 from glob import *
+import re
 
 f=glob("/Users/aleksandrgarshin/Seafile/p4ne_training/config_files/*.txt")
 
@@ -7,12 +8,11 @@ mylist=list("")
 for file in f:
     with open(file) as curfile:
         for curline in curfile:
-            if curline.find("ip address") != -1 and curline.find("no ip address") == -1:mylist.append(curline)
+            if blob(re.match("ip address ([0-9]+)\.([0-9]+)\.([0-9]+)\.([0-9])", curline)):mylist.append(curline)
 
 
 i=sorted(list(set(list(mylist))))
 
 
 for str2 in i:
-    str3=str2.strip()
-    print(str3.replace("ip address", ""))
+    print(str2.replace("ip address", "").strip())
