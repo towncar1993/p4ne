8ad4b39ab4be827fba6aebbdb7a712cdeacf76ff
aleksandrgarshin <mail@mail.ru>
2017-06-09 18:06:57 +0300

    lab1.4

--- Lab1.5/file.py
+++ Lab1.5/file.py
@@ -5,14 +5,28 @@ f=glob("/Users/aleksandrgarshin/Seafile/p4ne_training/config_files/*.txt")
 
 mylist=list("")
 
+def parce(str):
+    if re.search("ip address ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]) ([0-9]+\.[0-9]+\.[0-9]+\.[0-9])", str):
+        ip_addr=re.search("ip address ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]) ([0-9]+\.[0-9]+\.[0-9]+\.[0-9])", str).group(1)
+        subnet = re.search("ip address ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]) ([0-9]+\.[0-9]+\.[0-9]+\.[0-9])", str).group(2)
+        return ("IP", ip_addr, subnet)
+    if re.search("^interface ([a-zA-Z]+[0-9]+)", str):
+        interface_name=re.search("^interface ([a-zA-Z]+[0-9]+)", str).group(1)
+        return ("INT", interface_name)
+    if re.search("^hostname ([a-zA-Z]+)", str):
+        host_name=re.search("^hostname ([a-zA-Z]+)", str).group(1)
+        return ("HOST", host_name)
+    return "UNCLASSIFIED"
+
 for file in f:
     with open(file) as curfile:
         for curline in curfile:
-            if blob(re.match("ip address ([0-9]+)\.([0-9]+)\.([0-9]+)\.([0-9])", curline)):mylist.append(curline)
+            if parce(curline)[0] == "IP":mylist.append(parce(curline)[0]+" "+parce(curline)[1]+" "+parce(curline)[2])
+            if parce(curline)[0] == "INT":mylist.append(parce(curline)[0]+" "+parce(curline)[1])
+            if parce(curline)[0] == "HOST":mylist.append(parce(curline)[0]+" "+parce(curline)[1])
+
+#i=sorted(list(set(list(mylist))))
 
 
-i=sorted(list(set(list(mylist))))
-
-
-for str2 in i:
-    print(str2.replace("ip address", "").strip())
+for str2 in mylist:
+    print(str2.strip())
